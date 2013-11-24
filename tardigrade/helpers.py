d161de61ec7c5de8e1ad49777027781ff8884c76
sh4nks <sh4nks7@gmail.com>
2013-11-24 20:44:25 +0100

    Added two time filters for jinja

--- tardigrade/helpers.py
+++ tardigrade/helpers.py
@@ -8,6 +8,8 @@
     :copyright: (c) 2013 by the Tardigrade Team.
     :license: BSD, see LICENSE for more details.
 """
+from datetime import datetime
+
 from flask import session, current_app
 from flask.ext.themes2 import render_theme_template
 
@@ -15,3 +17,53 @@ from flask.ext.themes2 import render_theme_template
 def render_template(template, **context):
     theme = session.get('theme', current_app.config['DEFAULT_THEME'])
     return render_theme_template(theme, template, **context)
+
+
+def time_format(value, format='%Y-%m-%d'):
+    """
+    Returns a formatted time string
+    """
+    return value.strftime(format)
+
+
+def time_since(value):
+    """
+    Just a interface for `time_delta_format`
+    """
+    return time_delta_format(value)
+
+
+def time_delta_format(dt, default=None):
+    """
+    Returns string representing "time since" e.g.
+    3 days ago, 5 hours ago etc.
+    Ref: https://bitbucket.org/danjac/newsmeme/src/a281babb9ca3/newsmeme/
+    """
+
+    if default is None:
+        default = 'just now'
+
+    now = datetime.utcnow()
+    diff = now - dt
+
+    periods = (
+        (diff.days / 365, 'year', 'years'),
+        (diff.days / 30, 'month', 'months'),
+        (diff.days / 7, 'week', 'weeks'),
+        (diff.days, 'day', 'days'),
+        (diff.seconds / 3600, 'hour', 'hours'),
+        (diff.seconds / 60, 'minute', 'minutes'),
+        (diff.seconds, 'second', 'seconds'),
+    )
+
+    for period, singular, plural in periods:
+
+        if not period:
+            continue
+
+        if period == 1:
+            return u'%d %s ago' % (period, singular)
+        else:
+            return u'%d %s ago' % (period, plural)
+
+    return default
